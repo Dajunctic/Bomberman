a68f03933cd5a737bd3b3cebe567a08fd4c03836
Dajunctic Vu <dajuncticvu@gmail.com>
2022-09-20 21:55:08 +0700

    dajunctic fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module uet.oop.bomberman {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-    requires jfxrt;
 
     opens uet.oop.bomberman to javafx.fxml;
     exports uet.oop.bomberman;
