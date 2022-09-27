54e42c627da9ace3d20a41c9dce8f96d4d8ac727
NguyenVanHung24 <vanhung240403@gmail.com>
2022-09-28 00:10:39 +0700

    push init function again

--- src/main/java/uet/oop/bomberman/entities/Bomber.java
+++ src/main/java/uet/oop/bomberman/entities/Bomber.java
@@ -6,6 +6,7 @@ import javafx.scene.effect.Effect;
 import javafx.scene.effect.MotionBlur;
 import javafx.scene.image.Image;
 import javafx.scene.shape.Rectangle;
+import uet.oop.bomberman.Generals.Point;
 import uet.oop.bomberman.game.Gameplay;
 import uet.oop.bomberman.graphics.Anim;
 import uet.oop.bomberman.graphics.Sprite;
@@ -411,9 +412,71 @@ public class Bomber extends Mobile {
                         return;
                     }
                 }
+
             }
         }
 
         return;
+    }
+    @Override
+    public boolean checkCollision(double ref_x, double ref_y, int margin) {
+        // có đấy bạn ạ
+        if(ref_x < 0 || ref_y < 0
+                || ref_x > width * Sprite.SCALED_SIZE - this.getWidth()
+                || ref_y > height * Sprite.SCALED_SIZE - this.getHeight()) return true;
+
+        Rectangle rect;
+        if(mode == CENTER_MODE)
+            rect = new Rectangle(ref_x - this.getWidth() / 2 + margin, ref_y - this.getHeight() / 2 + margin, this.getWidth() - margin, this.getHeight() - margin);
+        else
+            rect = new Rectangle(ref_x, ref_y, this.getWidth(), this.getHeight());
+
+        // Không cần check ra khỏi map vì trong update BOMBER hoặc ENEMY sẽ giới hạn speed.
+
+        // Thay vì ngồi debug code Hưng fake thì tôi kiểm tra tất cả các tiles xung quanh thực thể luôn.
+        int tileStartX = (int) Math.max(0, Math.floor(rect.getX() / Sprite.SCALED_SIZE));
+        int tileStartY = (int) Math.max(0, Math.floor(rect.getY() / Sprite.SCALED_SIZE));
+        int tileEndX = (int) Math.ceil((rect.getX() + rect.getWidth()) / Sprite.SCALED_SIZE);
+        int tileEndY = (int) Math.ceil((rect.getY() + rect.getHeight()) / Sprite.SCALED_SIZE);
+        tileEndX = Math.min(tileEndX, Gameplay.width - 1);
+        tileEndY = Math.min(tileEndY, Gameplay.height - 1);
+        for (int i = tileStartX; i <= tileEndX; i++) {
+            for (int j = tileStartY; j <= tileEndY; j++) {
+
+                int tileX = i * Sprite.SCALED_SIZE;
+                int tileY = j * Sprite.SCALED_SIZE;
+
+                Rectangle tileRect = new Rectangle(tileX, tileY, Sprite.SCALED_SIZE, Sprite.SCALED_SIZE);
+
+                if (Gameplay.tile_map[j][i] > '0'&&Gameplay.tile_map[j][i] < '3') {
+                    if (Physics.collisionRectToRect(rect, tileRect)) {
+                        return true;
+                    }
+                }
+                if (Gameplay.tile_map[j][i] ==  '3') {
+                    if (Physics.collisionRectToRect(rect, tileRect)) {
+                        killTask.add(new Point(i,j));
+                        System.out.println("speed");
+                        return true;
+                    }
+                }
+                if (Gameplay.tile_map[j][i] == '4') {
+                    if (Physics.collisionRectToRect(rect, tileRect)) {
+                        killTask.add(new Point(i,j));
+                        return true;
+                    }
+                }
+                if (Gameplay.tile_map[j][i] == '5') {
+                    if (Physics.collisionRectToRect(rect, tileRect)) {
+
+                        killTask.add(new Point(i,j));
+                        return true;
+                    }
+                }
+
+            }
+        }
+
+        return false;
     }
 }
