b5d64ca033bd677299b7ed9c0aea72edad0ef487
OwOIamNoob <21020205@vnu.edu.vn>
2022-09-27 17:24:10 +0700

    Burning ground class

---
+++ src/main/java/uet/oop/bomberman/entities/Fire.java
@@ -0,0 +1,62 @@
+package uet.oop.bomberman.entities;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.Generals.Point;
+import uet.oop.bomberman.game.Gameplay;
+import uet.oop.bomberman.graphics.DeadAnim;
+import uet.oop.bomberman.graphics.Sprite;
+import uet.oop.bomberman.graphics.SpriteSheet;
+
+import static uet.oop.bomberman.game.Gameplay.tile_map;
+
+public class Fire extends Entity{
+
+    protected DeadAnim ignite = new DeadAnim(SpriteSheet.ignite, 1, 1);
+    protected DeadAnim fade = new DeadAnim(SpriteSheet.fire_fade, 3, 1);
+    protected DeadAnim burn;
+    int tilex,tiley;
+    public Fire(double xUnit, double yUnit) {
+        super(xUnit, yUnit);
+        x *= Sprite.SCALED_SIZE;
+        y *= Sprite.SCALED_SIZE;
+    }
+
+    public Fire(double xUnit, double yUnit, double duration){
+        super(xUnit, yUnit);
+        tilex = (int) xUnit;
+        tiley = (int) yUnit;
+        x *= Sprite.SCALED_SIZE;
+        y *= Sprite.SCALED_SIZE;
+        burn = new DeadAnim(SpriteSheet.fire, 2, duration);
+        tile_map[(int) yUnit][(int) xUnit] = '!';
+        kill();
+    }
+
+    @Override
+    public void update() {
+            if(!ignite.isDead()) ignite.update();
+                else if(!burn.isDead()) burn.update();
+                    else fade.update();
+    }
+
+    @Override
+    public Image getImg() {
+        if(!ignite.isDead()) return ignite.getImage();
+            else if(!burn.isDead()) return burn.getImage();
+                else return fade.getImage();
+    }
+
+    @Override
+    public boolean isExisted() {
+        return !fade.isDead();
+    }
+    @Override
+    public void deadAct(Gameplay gameplay) {
+        tile_map[tiley][tilex] = '0';
+    }
+
+    @Override
+    public void kill() {
+        Gameplay.killTask.add(new Point(tilex,tiley));
+    }
+}
